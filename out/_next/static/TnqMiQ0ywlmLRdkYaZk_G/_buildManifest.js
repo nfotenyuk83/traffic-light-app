8813e3ee9afc1dd0937952fdc33e8afd9fc1c917
Nikolay Fotenyuk <nikolay.fotenyuk@avanquest.com>
2025-10-06 16:28:59 -0400

    preparation to deployment

---
+++ out/_next/static/TnqMiQ0ywlmLRdkYaZk_G/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/pages/index-faa59b423c8e4438.js"],"/_error":["static/chunks/pages/_error-b6491f42fb2263bb.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
